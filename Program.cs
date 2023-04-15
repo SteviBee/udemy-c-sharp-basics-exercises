821543c8307bed7c8723050e2b37426faba78d45
Stephen Borutta <Stephen.Borutta.Dev@gmail.com>
2023-04-15 09:54:05 -0500

    adding exercises 5

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Collections.Generic;
+
+//Format quick key -> Option + shift + F
+//Option + shift + arrow -> copy below
+//Option+ command + arrow -> change window 
+
+//Entry point: NORMAL
+//namespace HelloWorld
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+namespace Section_5_Exercises
+{
+    public class Conditionals
+
+    {
+        static void Main(string[] args)
+
+        {
+
+            //1- Write a program and ask the user to enter a number.The number should be between 1 to 10.
+            //If the user enters a valid number, display "Valid" on the console.Otherwise, display "Invalid".
+            // (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+            static void AskNum()
+            {
+                Console.WriteLine("Enter number between 1-10: ");
+                //Create variable:
+                int input = Convert.ToInt32(Console.ReadLine());
+                if (input <= 10)
+                {
+                    Console.WriteLine("valid");
+                }
+                else
+                {
+                    Console.WriteLine("invalde");
+                }
+            }
+
+            //AskNum();
+
+            //2- Write a program which takes two numbers from the console and displays the maximum of the two.
+            static void TwoNums()
+            {
+                Console.WriteLine("Enter two number, and I will show max: ");
+                var spiltted = Console.ReadLine().Split(" ");
+                int inputOne = Convert.ToInt32(spiltted[0]);
+                int inputLast = Convert.ToInt32(spiltted[1]);
+
+                if (inputOne > inputLast)
+                {
+                    Console.WriteLine("Max is: {0}", inputOne);
+                }
+                else
+                {
+                    Console.WriteLine("Max is: {0}", inputLast);
+
+                }
+            }
+            //TwoNums();
+            // 3 - Write a program and ask the user to enter the width and height of an image.
+            //Then tell if the image is landscape or portrait.
+            static void WidthHeightFinder()
+            {
+                Console.WriteLine("enter width of an image: ");
+                var width = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter height of an image: ");
+                var height = Convert.ToInt32(Console.ReadLine());
+
+                var imageType = (width > height) ? "landscape" : "portrait";
+                Console.WriteLine(imageType);
+            }
+            //WidthHeightFinder();
+
+            //            4 - Your job is to write a program for a speed camera.
+            //For simplicity, ignore the details such as camera, sensors, etc and focus purely on the logic.
+            //Write a program that asks the user to enter the speed limit.Once set, the program asks for the speed of a car.
+            // If the user enters a value less than the speed limit, program should display Ok on the console.
+            //If the value is above the speed limit, the program should calculate the number of demerit points.
+            //For every 5km / hr above the speed limit, 1 demerit points should be incurred and displayed on the console.
+            // If the number of demerit points is above 12, the program should display License Suspended.
+            static void Other()
+            {
+                Console.Write("What is the speed limit? ");
+                var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("What is the speed of this car? ");
+                var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+                if (carSpeed < speedLimit)
+                    Console.WriteLine("Ok");
+                else
+                {
+                    const int kmPerDemeritPoint = 5;
+                    var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+                    if (demeritPoints > 12)
+                        Console.WriteLine("License Suspended");
+                    else
+                        Console.WriteLine("Demerit points: " + demeritPoints);
+                }
+
+
+
+            }
+
+
+
+        }
+
+    }
